2e6c07ca9256cf316deade7096a5934bd4fc884b
TanayPatil <73037585+TanayPatil2001@users.noreply.github.com>
2024-03-02 15:22:43 +0530

    Delete bank directory

--- bank/customers/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import customersDetailsModel
-
-
-class customerDetailForm(forms.ModelForm):
-    class Meta:
-        model = customersDetailsModel
-        fields = '__all__'
